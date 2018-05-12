7d93283f581fc4accbb164ec63904a4ad795c133
Jason Toms <jasonctoms@gmail.com>
2018-05-12 17:55:32 +0200

    Initial app structure.

---
+++ lib/nav_drawer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Drawer();
+  }
+
+}
---
+++ lib/salary_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:hairdresser_calc/nav_drawer.dart';
+
+class SalaryScreen extends StatefulWidget {
+  SalaryScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<SalaryScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      drawer: new NavDrawer(),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
